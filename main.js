f4aa43af0b09f42cc0007c468dd584344d1a51a1
Supergoed1 <40697454+Supergoed1@users.noreply.github.com>
2019-06-12 14:47:37 +0200

    f

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //PlainIncremental Code IGNORE
 var game = {
-    dataversion: 0.1,
     money: 0,
     moneyPerClick: 1,
     moneyPerSec: 0,
@@ -12,9 +11,6 @@ var game = {
     perSecUpgradeCost: 25,
     perSecAmountPerUpgrade: 1,
     perSecUpgradeAmountTillNextUpgrade: 0,
-    offlineTimeCost: 1000,
-    maxOfflineTime: 0.5,
-    prestigeBonusUpgradeCost: 500
 };
 var market = {
     appleprice: 10,
@@ -27,15 +23,15 @@ var dataversion = 0.1;
 var defaultGame = game;
 
 var upgradeMenu = document.getElementById("upgradesMenu");
-//var updateguiint = setInterval("updateGui()", 20);
+var updateguiint = setInterval("updateGui()", 20);
 //var upgradeint = setInterval("update()", 50);
 var everySec = setInterval(() => {
     for (let index = 0; index < subjects.length; index++) {
         var subject = subjects[index];
         
-        subject.game.money += (getPrestigeBonus(subject.game.moneyPerSec) / 100);
+        subject.aiGame.money += getPrestigeBonus(subject.aiGame.moneyPerSec, subject);
     }
-}, 10);
+}, 1000);
 
 function init() {
     AiInit();
@@ -43,21 +39,18 @@ function init() {
 }
 
 function reset(subject) {
-    subject.game = defaultGame;
+    subject.aiGame = defaultGame;
 }
-/*
+
 function updateGui() {
-    document.getElementById("money").innerHTML = format(subject.subject.game.money);
-    document.getElementById("presCoins").innerHTML = "Prestige Coins: " + subject.subject.game.prestigeCoins;
-    document.getElementById("presBonus").innerHTML = "Prestige Bonus: " + subject.subject.game.prestigeCoins * subject.subject.game.bonusPerPresCoin + "%";
-    document.getElementById("clickButton").innerHTML = "+" + format(getPrestigeBonus(subject.subject.game.moneyPerClick));
-    document.getElementById("clickUpgrade").innerHTML = "+" + format(subject.subject.game.clickAmountPerUpgrade) + "/click <br> Cost: " + format(subject.subject.game.clickUpgradeCost);
-    document.getElementById("perSecUpgrade").innerHTML = "+" + format(subject.subject.game.perSecAmountPerUpgrade) + "/per sec <br> Cost: " + format(subject.subject.game.perSecUpgradeCost);
-    document.getElementById("offlineTimeUpgrade").innerHTML = "+0.5 hours offline time <br> Cost: " + format(subject.subject.game.offlineTimeCost);
-    document.getElementById("aprice").innerHTML = "Apple Price: " + format(market.appleprice);
-    document.getElementById("aamount").innerHTML = "Apples: " + market.apples;
+    document.getElementById("sub1cash").innerHTML="Subject 1 cash:"+subjects[0].aiGame.money;
+    document.getElementById("sub2cash").innerHTML="Subject 2 cash:"+subjects[1].aiGame.money;
+    document.getElementById("sub3cash").innerHTML="Subject 3 cash:"+subjects[2].aiGame.money;
+    document.getElementById("sub4cash").innerHTML="Subject 4 cash:"+subjects[3].aiGame.money;
+    document.getElementById("sub5cash").innerHTML="Subject 5 cash:"+subjects[4].aiGame.money;
+    document.getElementById("sub6cash").innerHTML="Subject 6 cash:"+subjects[5].aiGame.money;
 }
-*/
+
 
 function toggleVisibility(element) {
     if(element.style.visibility == "hidden") {
@@ -68,11 +61,11 @@ function toggleVisibility(element) {
 }
 
 function getPrestigeBonus(num, subject) {
-    return (num * (1 +  (subject.game.prestigeCoins * subject.subject.game.bonusPerPresCoin / 100)));
+    return (num * (1 +  (subject.aiGame.prestigeCoins * subject.aiGame.bonusPerPresCoin / 100)));
 }
 
 function onClick(subject) {
-    subject.game.money = subject.game.money * 1 + getPrestigeBonus(subject.game.moneyPerClick);
+    subject.aiGame.money = subject.aiGame.money * 1 + getPrestigeBonus(subject.aiGame.moneyPerClick, subject);
 }
 
 function prestige(subject) {
@@ -81,18 +74,18 @@ function prestige(subject) {
         sellMarket("apple");
     }
     document.getElementById("applesellamount").value = 1;
-    subject.game.prestigeCoins += Math.round(Math.round(subject.game.money / 100));
-    subject.game.money = 0;
-    subject.game.moneyPerClick = 1;
-    subject.game.moneyPerSec = 0;
-    subject.game.clickAmountPerUpgrade = 1;
-    subject.game.clickUpgradeAmountTillNextUpgrade = 0;
-    subject.game.clickUpgradeCost = 10;
-    subject.game.perSecAmountPerUpgrade = 1;
-    subject.game.perSecUpgradeAmountTillNextUpgrade = 0;
-    subject.game.perSecUpgradeCost = 25;
-    subject.game.offlineTimeCost = 1000;
-    subject.game.maxOfflineTime = 0.5;
+    subject.aiGame.prestigeCoins += Math.round(Math.round(subject.aiGame.money / 100));
+    subject.aiGame.money = 0;
+    subject.aiGame.moneyPerClick = 1;
+    subject.aiGame.moneyPerSec = 0;
+    subject.aiGame.clickAmountPerUpgrade = 1;
+    subject.aiGame.clickUpgradeAmountTillNextUpgrade = 0;
+    subject.aiGame.clickUpgradeCost = 10;
+    subject.aiGame.perSecAmountPerUpgrade = 1;
+    subject.aiGame.perSecUpgradeAmountTillNextUpgrade = 0;
+    subject.aiGame.perSecUpgradeCost = 25;
+    subject.aiGame.offlineTimeCost = 1000;
+    subject.aiGame.maxOfflineTime = 0.5;
 }
 
 function format(number) {
@@ -101,27 +94,27 @@ function format(number) {
 
 function buyUpgrade(upgrade, subject) {
     if(upgrade == "click") {   
-        if(subject.game.money >= subject.game.clickUpgradeCost) {
-            subject.game.moneyPerClick += subject.game.clickAmountPerUpgrade;
-            subject.game.money -= subject.game.clickUpgradeCost;
-            subject.game.clickUpgradeAmountTillNextUpgrade += 1;
-            if(subject.game.clickUpgradeAmountTillNextUpgrade >= 5) {
-                subject.game.clickAmountPerUpgrade *= 2;
-                subject.game.clickUpgradeAmountTillNextUpgrade = 0;
+        if(subject.aiGame.money >= subject.aiGame.clickUpgradeCost) {
+            subject.aiGame.moneyPerClick += subject.aiGame.clickAmountPerUpgrade;
+            subject.aiGame.money -= subject.aiGame.clickUpgradeCost;
+            subject.aiGame.clickUpgradeAmountTillNextUpgrade += 1;
+            if(subject.aiGame.clickUpgradeAmountTillNextUpgrade >= 5) {
+                subject.aiGame.clickAmountPerUpgrade *= 2;
+                subject.aiGame.clickUpgradeAmountTillNextUpgrade = 0;
             }
-            subject.game.clickUpgradeCost *= 1.50;
+            subject.aiGame.clickUpgradeCost *= 1.50;
         }
     }
     if(upgrade == "perSec") {   
-        if(subject.game.money >= subject.game.perSecUpgradeCost) {
-            subject.game.moneyPerSec += subject.game.perSecAmountPerUpgrade;
-            subject.game.money -= subject.game.perSecUpgradeCost;
-            subject.game.perSecUpgradeAmountTillNextUpgrade += 1;
-            if(subject.game.perSecUpgradeAmountTillNextUpgrade >= 5) {
-                subject.game.perSecAmountPerUpgrade *= 2;
-                subject.game.perSecUpgradeAmountTillNextUpgrade = 0;
+        if(subject.aiGame.money >= subject.aiGame.perSecUpgradeCost) {
+            subject.aiGame.moneyPerSec += subject.aiGame.perSecAmountPerUpgrade;
+            subject.aiGame.money -= subject.aiGame.perSecUpgradeCost;
+            subject.aiGame.perSecUpgradeAmountTillNextUpgrade += 1;
+            if(subject.aiGame.perSecUpgradeAmountTillNextUpgrade >= 5) {
+                subject.aiGame.perSecAmountPerUpgrade *= 2;
+                subject.aiGame.perSecUpgradeAmountTillNextUpgrade = 0;
             }
-            subject.game.perSecUpgradeCost *= 1.50;
+            subject.aiGame.perSecUpgradeCost *= 1.50;
         }
     }
     
@@ -136,47 +129,186 @@ function getRandomInt(min, max) {
 init();
 
 //AI Starts here
+
+var stop = false;
+
 var aiInterval = setInterval(() => {
+    if(stop) {
+        return;
+    }
     for (let index = 0; index < subjects.length; index++) {
         var subject = subjects[index];
-        if(subject.done) {
+        if(checkIfDone()) {
+            evolve();
             return;
+        }
+        if(subject.done) {
+            continue;
         }
         if(subject.turns <= 0) {
             console.log("Subject " + subject.number + " is out of turns")
+            console.log("Subject earned " + subject.aiGame.money)
             subject.done = true;
-            return;
+            continue;
+        } else {
+            doAction(subject, getRandomInt(0,3))
         }
-        doAction(subject, getRandomInt(0,3))
     }
 }, 1000);
 
-var subjects = [subject1 = {turns: 5, aiGame: game, number: 1, done: false},
-subject2 = {turns: 5, aiGame: game, number: 2, done: false},
-subject3 = {turns: 5, aiGame: game, number: 3, done: false},
-subject4 = {turns: 5, aiGame: game, number: 4, done: false},
-subject5 = {turns: 5, aiGame: game, number: 5, done: false},
-subject6 = {turns: 5, aiGame: game, number: 6, done: false}];
+var subjects = [subject1 = {turns: 5,maxTurns: 5,path: [], aiGame: {
+    money: 0,
+    moneyPerClick: 1,
+    moneyPerSec: 0,
+    prestigeCoins: 0,
+    bonusPerPresCoin: 1,
+    clickUpgradeCost: 10,
+    clickAmountPerUpgrade: 1,
+    clickUpgradeAmountTillNextUpgrade: 0,
+    perSecUpgradeCost: 25,
+    perSecAmountPerUpgrade: 1,
+    perSecUpgradeAmountTillNextUpgrade: 0,
+}, number: 1, done: false},
+subject2 = {turns: 5,maxTurns: 5,path: [], aiGame: {
+    money: 0,
+    moneyPerClick: 1,
+    moneyPerSec: 0,
+    prestigeCoins: 0,
+    bonusPerPresCoin: 1,
+    clickUpgradeCost: 10,
+    clickAmountPerUpgrade: 1,
+    clickUpgradeAmountTillNextUpgrade: 0,
+    perSecUpgradeCost: 25,
+    perSecAmountPerUpgrade: 1,
+    perSecUpgradeAmountTillNextUpgrade: 0,
+}, number: 2, done: false},
+subject3 = {turns: 5,maxTurns: 5,path: [], aiGame: {
+    money: 0,
+    moneyPerClick: 1,
+    moneyPerSec: 0,
+    prestigeCoins: 0,
+    bonusPerPresCoin: 1,
+    clickUpgradeCost: 10,
+    clickAmountPerUpgrade: 1,
+    clickUpgradeAmountTillNextUpgrade: 0,
+    perSecUpgradeCost: 25,
+    perSecAmountPerUpgrade: 1,
+    perSecUpgradeAmountTillNextUpgrade: 0,
+}, number: 3, done: false},
+subject4 = {turns: 5,maxTurns: 5,path: [], aiGame: {
+    money: 0,
+    moneyPerClick: 1,
+    moneyPerSec: 0,
+    prestigeCoins: 0,
+    bonusPerPresCoin: 1,
+    clickUpgradeCost: 10,
+    clickAmountPerUpgrade: 1,
+    clickUpgradeAmountTillNextUpgrade: 0,
+    perSecUpgradeCost: 25,
+    perSecAmountPerUpgrade: 1,
+    perSecUpgradeAmountTillNextUpgrade: 0,
+}, number: 4, done: false},
+subject5 = {turns: 5,maxTurns: 5,path: [], aiGame: {
+    money: 0,
+    moneyPerClick: 1,
+    moneyPerSec: 0,
+    prestigeCoins: 0,
+    bonusPerPresCoin: 1,
+    clickUpgradeCost: 10,
+    clickAmountPerUpgrade: 1,
+    clickUpgradeAmountTillNextUpgrade: 0,
+    perSecUpgradeCost: 25,
+    perSecAmountPerUpgrade: 1,
+    perSecUpgradeAmountTillNextUpgrade: 0,
+}, number: 5, done: false},
+subject6 = {turns: 5,maxTurns: 5,path: [], aiGame: {
+    money: 0,
+    moneyPerClick: 1,
+    moneyPerSec: 0,
+    prestigeCoins: 0,
+    bonusPerPresCoin: 1,
+    clickUpgradeCost: 10,
+    clickAmountPerUpgrade: 1,
+    clickUpgradeAmountTillNextUpgrade: 0,
+    perSecUpgradeCost: 25,
+    perSecAmountPerUpgrade: 1,
+    perSecUpgradeAmountTillNextUpgrade: 0,
+}, number: 6, done: false}];
 
 function AiInit() {
     console.log("AI init complete");
 }
 
+function checkIfDone() {
+    var donesubjects = 0;
+    for (let index = 0; index < subjects.length; index++) {
+        var subject = subjects[index];
+        if(subject.done) {
+            donesubjects++;
+            continue;
+        }
+    }
+    if(donesubjects >= subjects.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function evolve() {
+    stop = true;
+    for (let index = 0; index < subjects.length; index++) {
+        var subject = subjects[index];
+        console.log("Subject " +  subject.number  + " earned " + subject.aiGame.money)
+        subject.maxTurns += 5;
+        subject.turns = subject.maxTurns;
+        subject.done = false;
+    }
+    var stoptimeout = setTimeout(() => {
+        for (let index = 0; index < subjects.length; index++) {
+            var subject = subjects[index];
+            subject.aiGame = {
+                money: 0,
+                moneyPerClick: 1,
+                moneyPerSec: 0,
+                prestigeCoins: 0,
+                bonusPerPresCoin: 1,
+                clickUpgradeCost: 10,
+                clickAmountPerUpgrade: 1,
+                clickUpgradeAmountTillNextUpgrade: 0,
+                perSecUpgradeCost: 25,
+                perSecAmountPerUpgrade: 1,
+                perSecUpgradeAmountTillNextUpgrade: 0,
+            }
+        }
+        stop = false;
+    }, 3000);
+}
+
 function doAction(subject,action) {
     //0 = click, 1 = buyClickUpgrade, 2 = buy perSecUpgrade
+    if(subject.turns <= 0) {
+        subject.done = true;
+        return;
+    }
     switch (action) {
         case 0:
             onClick(subject);
             subject.turns--;
             console.log("Subject " + subject.number + " clicked the button");
+            subject.path.push(action);
             break;
         case 1:
             buyUpgrade("click", subject);
             subject.turns--;
+            console.log("Subject " + subject.number + " bought click upgrade");
+            subject.path.push(action);
             break;
         case 2:
             buyUpgrade("perSec", subject);
             subject.turns--;
+            console.log("Subject " + subject.number + " bought perSec upgrade");
+            subject.path.push(action);
             break;
     }
 }
